32834be8f3a135e18d7041e6ac49b8b6a8184d93
Boris Parak <parak@cesnet.cz>
2015-01-28 15:06:24 +0100

    Added support for NoSQL MongoDB

--- lib/onesnooper_server/stores/mongodb_store.rb
+++ lib/onesnooper_server/stores/mongodb_store.rb
@@ -4,8 +4,20 @@ require 'mongo'
 # document structure.
 class OnesnooperServer::Stores::MongodbStore < ::OnesnooperServer::Store
 
+  # constant collection name
+  MONGO_COLL_NAME = 'one_monitoring'
+
+  def initialize(params = {})
+    super
+    @db_conn = ::Mongo::MongoClient.new(params[:host], params[:port])
+    @db_active_db = @db_conn.db(params[:database])
+    @db_coll = @db_active_db.create_collection(MONGO_COLL_NAME)
+  end
+
   def save!(timestamp, data)
     ::OnesnooperServer::Log.debug "[#{self.class.name}] Saving #{timestamp.to_s} => #{data.inspect}"
+    data['TIMESTAMP'] = timestamp.to_time.utc
+    @db_coll.insert data
   end
 
 end
